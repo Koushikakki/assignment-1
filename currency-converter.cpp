17c72d5d7743dd2986e12e60a0d71836f502b44c
admin <admin@MacBookPro.lan>
2025-07-03 16:30:12 +0530

    added currency converter and take input from user

---
+++ currency-converter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double amount;
+    string from, to;
+    
+    cout << "Enter currency from-to (e.g. 100 USD INR): ";
+    cin >> amount >> from >> to;
+
+    
+
+    return 0;
+}
