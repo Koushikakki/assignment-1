67936a3ee759cb0be67409792d699f80217c692d
admin <admin@MacBookPro.lan>
2025-07-03 16:31:39 +0530

    added function to convert currency from USD to INR

--- currency-converter.cpp
+++ currency-converter.cpp
@@ -6,6 +6,10 @@ int main(){
     
     cout << "Enter currency from-to (e.g. 100 USD INR): ";
     cin >> amount >> from >> to;
+    if ((from == "USD" || from == "usd") && (to == "INR" || to == "inr")) {
+        cout << "Converted amount: " << amount * 85.69 << " INR" << endl;
+    }
+    
 
     
 
